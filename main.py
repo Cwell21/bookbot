be6e163d4a72b8fa0160bb80cc7b1ef2559552fb
Cwell21 <cwe11@yahoo.com>
2025-03-29 06:13:10 -0600

    Refactor code for error checking

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from stats import get_num_words
-
+from stats import get_num_words, get_book_text
 def main():
     # print(get_book_text('./books/frankenstein.txt'))
-    doc_word_count = get_num_words('./books/frankenstein.txt')
+    full_text = get_book_text('./books/frankenstein.txt')
+    doc_word_count = get_num_words(full_text)
     print(f'{doc_word_count} words found in the document')
 
 
--- stats.py
+++ stats.py
@@ -4,8 +4,11 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
-def get_num_words(file_path):
-    with open(file_path) as f:
-        document_text = f.read()
-    word_count = len(document_text.split())
+def get_num_words(document_text):
+    if isinstance(document_text, str):
+        word_count = len(document_text.split())
+    else:
+        raise ValueError('Input must be a string')
     return word_count
+
+
