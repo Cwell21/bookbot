176e7db39fd790e1a474706fc59222c0bbd1b2f1
Cwell21 <cwe11@yahoo.com>
2025-03-29 06:30:28 -0600

    Added character count function

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from stats import get_num_words, get_book_text
+from stats import get_num_words, get_book_text, get_char_count
 def main():
     # print(get_book_text('./books/frankenstein.txt'))
+
     full_text = get_book_text('./books/frankenstein.txt')
+    
     doc_word_count = get_num_words(full_text)
+    char_count = get_char_count(full_text)
+
     print(f'{doc_word_count} words found in the document')
+    print(char_count)
 
 
 main()
--- stats.py
+++ stats.py
@@ -11,4 +11,23 @@ def get_num_words(document_text):
         raise ValueError('Input must be a string')
     return word_count
 
+def get_char_count(document_text):
+    char_count = {}
+
+    if isinstance(document_text, str):
+        words = document_text.split()
+        for word in words:
+            for char in word:
+
+                char = char.lower()
+
+                if char in char_count:
+                    char_count[char] += 1
+                else:
+                    char_count[char] = 1    
+    else:
+        raise ValueError('Input must be a string')
+
+
+    return char_count
 
